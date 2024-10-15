424ef2c622d81ffac24de852a39ac8a2964712f1
Tomas Salas Arancibia <tsalasarancibia@gmail.com>
2024-10-15 11:04:38 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Introduction 
+Proyecto realizado para la landing page de la web de RakinTec
+
+# Tecnologías utilizadas
+- HTML
+- CSS
+- JavaScript
+- Tailwind
+
+# Desarrollo
+Para el desarrollo del proyecto se utilizó VSCode, y se ha utilizado la extensión Tailwind CSS para facilitar la creación de los estilos.
+
+# Despliegue
+Se ha utilizado Web Estatica de Azure para desplegar el proyecto.
+
+# Proximos pasos
+- Implementar el formulario de contacto
+- Manejar de mejor forma la CDN de Tailwind
+
+# Mejoras
+- Tamaño de la imagen de perfil en la página de nostros
---
+++ index.js
@@ -0,0 +1,60 @@
+window.onload = function() {
+  document.getElementById("inicio").scrollIntoView();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.getElementById('menu-toggle')
+  const mobileMenu = document.getElementById('mobile-menu')
+  let menuOpen = false
+
+  menuToggle.addEventListener('click', () => {
+    menuOpen = !menuOpen
+    if (menuOpen) {
+      mobileMenu.style.maxHeight = `${mobileMenu.scrollHeight}px`
+    } else {
+      mobileMenu.style.maxHeight = '0px'
+    }
+  })
+
+  const navLinks = document.querySelectorAll('.nav-link')
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault()
+      const targetId = e.target.getAttribute('href')
+      const targetSection = document.querySelector(targetId)
+
+      if (targetSection && document.startViewTransition) {
+        document.startViewTransition(() => {
+          window.scrollTo({
+            top: targetSection.offsetTop - 50,
+            behavior: 'smooth'
+          })
+        })
+      } else {
+        window.scrollTo({
+          top: targetSection.offsetTop - 50,
+          behavior: 'smooth'
+        })
+      }
+
+      if (menuOpen) {
+        menuOpen = false
+        mobileMenu.style.maxHeight = '0px'
+      }
+    })
+  })
+})
+
+const formulario = document.querySelector('form');
+formulario.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const nombre = document.getElementById('nombre').value;
+  const apellido = document.getElementById('apellido').value;
+  const email = document.getElementById('email').value;
+  const mensaje = document.getElementById('mensaje').value;
+
+  const datos = { nombre, apellido, email, mensaje };
+
+  console.log(datos)
+});
